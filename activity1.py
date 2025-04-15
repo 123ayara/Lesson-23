414c257df78470951ee4832d93882b9a8a5a92cf
Ayara <sushma.paudel4317@gmail.com>
2025-04-14 19:01:13 -0600

    first commit

---
+++ activity1.py
@@ -0,0 +1,9 @@
+student_info={"id1":{"name":"Lila","class":"5", "subj_integer":["english", "math", "science"
+"id2":{"name":"Kate","class":"5", "subj_integer":["english", "math", "science"]}}
+"id3":{"name":"Lisa","class":"6", "subj_integer":["english", "math", "science"]}}
+"id4":{"name":"Janet","class":"6", "subj_integer":["english", "math", "science"]}
+result={}
+for key, value in student_info.items():
+    if value not in result.values():
+        result[key]=value
+        print(value)
---
+++ activity3.py
@@ -0,0 +1,3 @@
+country_code={"India":"091","Austrailia":"0025", "Nepal":"0097"}
+print("The country code for India:",country_code.get("India", "not found"))
+print("The country code for Japan:",country_code.get("Japan", "not found"))
